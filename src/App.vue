d3fde3816693e1439da08c9732c7445510ad0871
Djordje <djordje3d@gmail.com>
2026-02-21 01:03:19 +0100

    Pojasnjenja1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref } from 'vue'
+import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
+const image = ref(socksGreenImage)
 
 </script>
 
@@ -9,7 +11,8 @@ const product = ref('Socks')
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
-        <!-- image goes here -->
+        <!-- <img v-bind:src="socksGreenImage"> --> this is the original static image
+        <img v-bind:src="image"> this is the image that is being displayed by the image variable
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
